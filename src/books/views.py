e0294a3e534c5ad8cca2da5657e9c44667cc7d5f
sasuke13 <bgnydyn@gmail.com>
2023-11-23 15:01:08 +0200

    refactor: uncomented BookAPIVew logic

--- src/books/views.py
+++ src/books/views.py
@@ -5,8 +5,7 @@ from rest_framework.views import APIView
 from books.exceptions import BookDoesNotExist
 from books.serializers import BookDetailedViewDTOSerializer, BookListViewDTOSerializer
 from core.base_api import ApiBaseView
-from core.containers import BookContainer, VisitorContainer
-from core.tasks import get_statistic_for_the_last_month
+from core.containers import BookContainer
 
 
 class BookAPIVew(APIView, ApiBaseView):
@@ -17,23 +16,18 @@ class BookAPIVew(APIView, ApiBaseView):
     book_interactor = BookContainer.interactor()
 
     def get(self, request, book_id: int = None):
-    #     if book_id:
-    #         try:
-    #             book = self.book_interactor.get_book_dto_by_id(book_id)
-    #
-    #         except BookDoesNotExist as exception:
-    #             return self._create_response_not_found(exception)
-    #
-    #         serialized_book = BookDetailedViewDTOSerializer(book)
-    #
-    #     else:
-    #         book = self.book_interactor.get_all_books_dto()
-    #
-    #         serialized_book = BookListViewDTOSerializer(book, many=True)
-    #
-    #     return Response({'book': serialized_book.data}, status=status.HTTP_200_OK)
-        cont = VisitorContainer.interactor()
-        cont.change_total_reading_time_for_the_last_week()
-        cont.change_total_reading_time_for_the_last_month()
+        if book_id:
+            try:
+                book = self.book_interactor.get_book_dto_by_id(book_id)
 
-        return Response(status=status.HTTP_200_OK)
+            except BookDoesNotExist as exception:
+                return self._create_response_not_found(exception)
+
+            serialized_book = BookDetailedViewDTOSerializer(book)
+
+        else:
+            book = self.book_interactor.get_all_books_dto()
+
+            serialized_book = BookListViewDTOSerializer(book, many=True)
+
+        return Response({'book': serialized_book.data}, status=status.HTTP_200_OK)
